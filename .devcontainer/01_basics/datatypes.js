b1045e74cdf05bf10f7ea332b5f115b0620c9b79
Abhishek Kumar <abhishek025it@gmail.com>
2026-06-02 09:01:16 +0000

    done

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,17 @@
+let name = "Abhishek"
+let age = 18
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// sysmbol => unique
+ 
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,5 @@
+const studentId = 2422025
+let studentName = "Abhishek"
+var studentGmail = "abhishekit@gmail.com"
+console.log(studentId, studentName, studentGmail);
+console.table([studentId, studentName, studentGmail])
