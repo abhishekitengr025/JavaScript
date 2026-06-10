6132d44761c7a1216bacb21a361b7b6a2e11bb98
Abhishek Kumar <abhishek025it@gmail.com>
2026-06-10 08:53:50 +0000

    compare value

---
+++ .devcontainer/01_basics/comparision.js
@@ -0,0 +1,24 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === comparision ke sath sath datatype bhi check krta h
+
+console.log("2" === 2);
+
+
+
+
